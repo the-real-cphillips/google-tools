0e222e720a358b77cf82f5905a6e05b65bbcff37
Craig Phillips <cphillips@2u.com>
2019-01-07 09:30:19 -0500

    [Feature Add/Remove] Add Gather/Archive, remove Label - `ThePurge`   - Added a new print statement to output amount of messages   - updated the docstring   - updated a conditional inside `purge` class method to handle the `archive` option, this was not previously implemented.   - remove `get_label_id` class method   - Update other locations where label was being handled.
    - `purge.py`
      - Similar changes above to reference the label removal and archive
    addition.
      - included new conditonal for `gather` inside `main`
      - updated locations where `archive` needed to be included
        - `main`
        - `validate_action_input`
    
    - Update `TODO`
    - Update `README`

--- gmail-purge/TODO.md
+++ gmail-purge/TODO.md
@@ -1,3 +1,4 @@
-- Determine if `label` related actions are needed
+- ~Determine if `label` related actions are needed~
+- ~Include `archive` for non batch processes~
 - Support `threads` as well as messages.
 - Write test around all this.
--- gmail-purge/purge.py
+++ gmail-purge/purge.py
@@ -9,8 +9,8 @@ def validate_action_input(value):
     in the argparse setup below
     """
 
-    if value.lower() not in [ 'archive', 'delete', 'trash' ]:
-        raise argparse.ArgumentTypeError(f'[X] Invalid Value: {value}\nPlease use "archive", "delete" or "trash"')
+    if value.lower() not in [ 'archive', 'delete', 'gather', 'trash' ]:
+        raise argparse.ArgumentTypeError(f'[X] Invalid Value: {value}\nPlease use "archive", "delete", "gather" or "trash"')
     return value
 
 
@@ -27,31 +27,26 @@ def main():
     auth = ThePurge.auth(scope=args.scope) 
     messages = ThePurge.Purge(auth)
 
-    if args.label_name:
-        label_id = messages.get_label_id(label_name=args.label_name)
-        messages.gather(query=args.query, label=label_id)
-    else:
-        messages.gather(query=args.query)
-
+    messages.gather(query=args.query)
     gather_output = messages.to_process
     data = messages.to_process
-
-    if len(data) > 1000 and args.action.lower() != 'trash':
-        check = None
-        while check is None:
-            bulk_input = input(f'[?] There are {len(data)} messages\n[?] Would you like to process these in bulk? (Y/N): ')
-            if validate_bulk_input(bulk_input) is True:
-                print("[I] Processing in Bulk")
-                messages.batch_process(data, args.action.lower())
-                check = True
-            elif validate_bulk_input(bulk_input) is False:
-                print("[I] Processing Normally")
-                messages.purge(data, args.action.lower())
-                check = True
-            else:
-                check = None
-    else:
-        messages.purge(data, args.action.lower())
+    if args.action.lower() != 'gather': 
+        if len(data) > 1000 and args.action.lower() != 'trash':
+            check = None
+            while check is None:
+                bulk_input = input(f'[?] There are {len(data)} messages\n[?] Would you like to process these in bulk? (Y/N): ')
+                if validate_bulk_input(bulk_input) is True:
+                    print("[I] Processing in Bulk")
+                    messages.batch_process(data, args.action.lower())
+                    check = True
+                elif validate_bulk_input(bulk_input) is False:
+                    print("[I] Processing Normally")
+                    messages.purge(data, args.action.lower())
+                    check = True
+                else:
+                    check = None
+        else:
+            messages.purge(data, args.action.lower())
 
 
 if __name__ == '__main__':
@@ -63,15 +58,11 @@ if __name__ == '__main__':
                         action='store',
                         default=None,
                         required=True,
-                        help="Choose: archive, delete, trash")
+                        help="Choose: archive, delete, gather, trash")
     parser.add_argument('-q', '--query',
                         dest='query',
                         action='store',
                         help="Enter your query string ie: older_than:30d, Use standard gmail queries")
-    parser.add_argument('-l', '--label_name',
-                        dest='label_name',
-                        action='store',
-                        help="label name to pass to the gather_label_id function")
     parser.add_argument('-b', '--bulk-process',
                         dest='bulk_process',
                         action='store_true',
